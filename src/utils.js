2cf24f44b488919226c284e160d5584e8774f0d1
Shubham Gupta <61898203+shubhamgithub21@users.noreply.github.com>
2021-06-26 23:13:56 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,3 @@
-/*
-    This file contains the common utility functions that will be used in our App at several places
-*/
-
-/* Return document height */
 function getDocumentHeight() {
 	const body = document.body;
 	const html = document.documentElement;
@@ -10,19 +5,16 @@ function getDocumentHeight() {
 	return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 }
 
-/* Return scrollTop height: The pixels value which is above the visible scrollable area */
 function getScrollTop() {
 	return window.pageYOffset !== undefined
 		? window.pageYOffset
 		: (document.documentElement || document.body.parentNode || document.body).scrollTop;
 }
 
-/* Check if scroll reached bottom */
 export function scrollAreaAvailable() {
 	return getScrollTop() < getDocumentHeight() - window.innerHeight;
 }
 
-/* Debounce function to discard a number of fastpace events */
 export function debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
@@ -39,7 +31,6 @@ export function debounce(func, wait, immediate) {
 	};
 }
 
-/* Throttle function for constant execution of a function after every x secs */
 export function throttle(fn, threshhold, scope) {
 	threshhold || (threshhold = 250);
 	var last, deferTimer;
@@ -61,7 +52,6 @@ export function throttle(fn, threshhold, scope) {
 	};
 }
 
-/* Check HTTP status if its ok */
 export function checkHttpStatus(response) {
 	if (response.status >= 200 && response.status < 300) {
 		return response;
@@ -70,12 +60,10 @@ export function checkHttpStatus(response) {
 	}
 }
 
-/* To parse response to JSON object */
 export function parseJSON(response) {
 	return response.json();
 }
 
-/* Generate image url */
 export function getImageUrl(farm, server, id, secret) {
 	return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
 }
